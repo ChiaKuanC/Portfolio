df8f8f40e74180b8355d4858b863ec52b5e1fb79
ChiaKuan <chiakuan.chen@gmail.com>
2018-10-27 16:20:46 +0800

    carousel

--- src/js/all.js
+++ src/js/all.js
@@ -1,3 +1,20 @@
-$(function() {
-
+$(function () {
+  function carousel() {
+    let carouselWidth = $('.portfolio .works').width();
+    let singleWidth = carouselWidth / 3;
+    let marginLeftNow = parseInt($('.portfolio .works').css('margin-left'));
+    console.log(marginLeftNow-singleWidth)
+    if (marginLeftNow < -(singleWidth * 2 - 25)) {
+      $('.portfolio .works').css({
+        'margin-left': '-25px'
+      });
+    } else {
+      $('.portfolio .works').css({
+        'margin-left': (marginLeftNow - singleWidth) + 'px'
+      });
+    }
+    setInterval(function () {
+      carousel()
+    }, 8000);
+  }
 });
